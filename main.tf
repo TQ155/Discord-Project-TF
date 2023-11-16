e30df9621dcbf44706b42a0fc1d6b72d611c2dca
TQ155 <tariq1992remastered@gmail.com>
2023-11-16 17:15:52 +0000

    one more 7

--- main.tf
+++ main.tf
@@ -24,8 +24,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-12345678"
+resource "aws_instance" "ec2-instance" {
+  ami           = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
 
 }
