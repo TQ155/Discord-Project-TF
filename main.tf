401d46451312f1444097556f78df5c2f7f708c29
TQ155 <tariq1992remastered@gmail.com>
2023-11-16 17:32:41 +0000

    one more 9

--- main.tf
+++ main.tf
@@ -27,5 +27,11 @@ provider "aws" {
 resource "aws_instance" "ec2-instance" {
   ami           = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
+  tags = {
+    Name = "TF Discord"
+    Environment = "Development"  
+    Owner = "YourName"
+    
+  }
 
 }
